90741d216f2fdea14d9941989ed60123f43170ac
muco <mucotresor@gmail.com>
2020-12-28 18:17:41 +0200

    setup: Add App view

---
+++ src/views/app/App.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import { Box, Heading } from '@chakra-ui/react';
+
+export const AppView = () => {
+    return (
+        <Box>
+            <Heading>App view</Heading>
+        </Box>
+    );
+};
